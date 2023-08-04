5ebe80a9e03d52c33314d7d439e414a3cecf962a
Shahid <shahid451998@gmail.com>
2023-08-04 22:50:00 +0530

    second commit

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ api =Api(app)
 slack_event_api = SlackEventAdapter('e435a3403c117be73a76c7588aa5aaaa' , '/slack/events', app)
 #k
 
-client =slack.WebClient(token='xoxb-5674230910401-5664059319844-aojoIoBknya7Lgal7hXfj6eh')
+client =slack.WebClient(token='xoxb-5674230910401-5664059319844-ygtH0xJgAyr3S7nvdg0qTb3H')
 
 @slack_event_api.on('message')
 def message(payload):
     event = payload.get('event',{})
     channel_id = event.get('channel')
     user_id = event.get('user')
+
     text = event.get('text')
+    print(text)
     client.chat_postMessage(channel=channel_id,text=text)
 
 
