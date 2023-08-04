b7fba8ca3d5eede48920287f3f64837752d07ed1
Shahid <shahid451998@gmail.com>
2023-08-04 23:06:38 +0530

    second commit

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from slackeventsapi import SlackEventAdapter
 from flask_restful import Api,Resource
 app = Flask(__name__)
 api =Api(app)
-slack_event_api = SlackEventAdapter('e435a3403c117be73a76c7588aa5aaaa' , '/slack/events', app)
+slack_event_api = SlackEventAdapter('6e759bbe36cd3532205c0729cd53e1a5' , '/slack/events', app)
 #k
 
-client =slack.WebClient(token='xoxb-5674230910401-5664059319844-ygtH0xJgAyr3S7nvdg0qTb3H')
+client =slack.WebClient(token='xoxb-5679288130871-5693863167794-ppzuG4iELTfp2JIaJB7FkrxX')
 
-@slack_event_api.on('message')
-def message(payload):
-    event = payload.get('event',{})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-
-    text = event.get('text')
-    print(text)
-    client.chat_postMessage(channel=channel_id,text=text)
+# @slack_event_api.on('message')
+# def message(payload):
+#     event = payload.get('event',{})
+#     channel_id = event.get('channel')
+#     user_id = event.get('user')
+#
+#     text = event.get('text')
+#     print(text)
+#     client.chat_postMessage(channel=channel_id,text=text)
 
 
 
@@ -27,7 +27,7 @@ class Check(Resource):
       print(request.json['question'])
      # r=requests.post('  https://f9c3-202-8-112-91.ngrok-free.app/Ai/askQuestion', json ={'question':request.json['question']})
       # print(r.json()['answer'])
-      client.chat_postMessage(channel='#gopro', text='hello')
+      client.chat_postMessage(channel='#test', text='hello')
       return {'message':'success'}
 
 api.add_resource(Check,'/get')
