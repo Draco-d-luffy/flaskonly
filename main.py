3d58b20cd2b96c93afe37b19d21b44148ef09ea9
Shahid <shahid451998@gmail.com>
2023-08-04 17:20:31 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import slack
+import requests
+from flask import Flask, request, Response
+from slackeventsapi import SlackEventAdapter
+from flask_restful import Api,Resource
+app = Flask(__name__)
+api =Api(app)
+slack_event_api = SlackEventAdapter('e435a3403c117be73a76c7588aa5aaaa' , '/slack/events', app)
+#k
+
+client =slack.WebClient(token='xoxb-5674230910401-5664059319844-aojoIoBknya7Lgal7hXfj6eh')
+
+class Check(Resource):
+  def post(self):
+      print(request.json['question'])
+      r=requests.post('http://127.0.0.1:8000/Ai/askQuestion', json ={'question':request.json['question']})
+      print(r.json()['answer'])
+      return {'message':'success'}
+
+api.add_resource(Check,'/get')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
